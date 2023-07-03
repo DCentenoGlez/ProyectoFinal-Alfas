0851c59d03977244c754f544780982b2cbedc610
Diego Ivan Centeno <dcenteno11@alumnos.uaq.mx>
2023-07-02 19:18:39 -0600

    Se pulieron detalles del programa
    -Se agregó un boton de para cerrar sesion en el menu principial
    -Se mejoraron los estilos en los botones, dandoles hover, para que se pueda apreciar mejor al momento de darle click
    -Se validó que si haya datos en los formularios antes de enviarlos
    -En las paginas de los formularios se agrego el boton de regresar al menu principal

--- script.js
+++ script.js
@@ -3,30 +3,54 @@ function realizarAccion() {
   
     switch (accion) {
       case "opcion1":
-        // Redireccionar al archivo HTML para asignar certificado
         window.location.href = "Asignar_certificado.html";
         break;
       case "opcion2":
-        // Redireccionar al archivo HTML para registrar alumno
         window.location.href = "Registrar_alumno.html";
         break;
       case "opcion3":
-        // Redireccionar al archivo HTML para ver lista de alumnos
         window.location.href = "Lista_alumnos.html";
         break;
       default:
-        // Acción por defecto si no se selecciona ninguna opción válida
         console.log("Acción no válida");
         break;
     }
   }
 
-  function enviarFormulario(event) {
-    console.log("Enviando formulario")
-    event.preventDefault(); // Evitar el envío del formulario por defecto
-    window.location.href = "Menu_administrador.html";
-    window.alert("Se enviaron los datos correctamente");
+  function cerrarSesion() {
+    // Eliminar el indicador de sesión iniciada del LocalStorage
+    localStorage.removeItem('sesionIniciada');
+    // Redireccionar a la página de inicio de sesión
+    window.location.href = 'Inicio_sesion.html';
   }
+
+  // Este envio de formulario es para Asignar certificado
+  function enviarFormulario(event) {
+    event.preventDefault(); // Evitar el envío del formulario por defecto
+
+    // Validar el formulario antes de enviarlo
+    if (validarFormulario()) {
+        console.log("Enviando formulario");
+        window.location.href = "Menu_administrador.html";
+        window.alert("Se enviaron los datos correctamente");
+    } else {
+        window.alert("No se pudo enviar el formulario porque no había información para registrar.");
+    }
+}
+
+// Este envio de formulario es para Registrar alumno
+function enviarFormulario2(event) {
+  event.preventDefault(); // Evitar el envío del formulario por defecto
+
+  // Validar el formulario antes de enviarlo
+  if (validarFormulario2()) {
+      console.log("Enviando formulario");
+      window.location.href = "Menu_administrador.html";
+      window.alert("Se enviaron los datos correctamente");
+  } else {
+      window.alert("No se pudo enviar el formulario porque no había información para registrar.");
+  }
+}
 
   function borrarFormulario() {
     document.getElementById("expediente").value = "";
@@ -35,4 +59,40 @@ function realizarAccion() {
     document.getElementById("materia").selectedIndex = 0;
   }
 
+// Validacion para Asignar certificado
+  function validarFormulario() {
+    var expediente = document.getElementById("expediente").value;
+    var certificado = document.getElementById("certificado").value;
+    var inicio = document.getElementById("inicio").value;
+    var termino = document.getElementById("termino").value;
+
+    // Verificar que los campos requeridos no estén vacíos
+    if (expediente === "" || certificado === "" || inicio === "" || termino === "") {
+        alert("Por favor, complete todos los campos requeridos.");
+        return false; 
+    }
+
+    return true; 
+}
+
+//Validacion para Registrar alumno
+function validarFormulario2() {
+  var expediente = document.getElementById("expediente").value;
+  var nombre = document.getElementById("nombre").value;
+  var apellidos = document.getElementById("apellidos").value;
+  var materia = document.getElementById("materia").value;
+
+  // Verificar que los campos requeridos no estén vacíos
+  if (expediente === "" || nombre === "" || apellidos === "" || materia === "") {
+      alert("Por favor, complete todos los campos requeridos.");
+      return false;
+  }
+
+  return true;
+}
+
+
+  function redireccionar(url) {
+    window.location.href = url;
+}
   
