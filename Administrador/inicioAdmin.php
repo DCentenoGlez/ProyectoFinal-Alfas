063180c62ce87f57737fcf573da622ff6a66759e
Carlos Zahid <zahidhdez@outlook.com>
2023-07-04 11:36:35 -0600

    hola3

--- Administrador/inicioAdmin.php
+++ Administrador/inicioAdmin.php
@@ -1,18 +1,63 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../estilo.css">
+    <script src="validacion.js"></script>
 </head>
 <?php
 $id = $_POST['id'];
 ?>
 <body>
-    <div class="contenedor">
-        <form method="POST" action="registrarAlumno.php">
-            <input type="submit" value="Registrar Alumno">
-        </form>
-    </div>
+    <header>
+        <div class="avatar">
+            <img src="https://cdn.icon-icons.com/icons2/2506/PNG/512/user_icon_150670.png" alt="Avatar">
+            <span>Nombre de Usuario</span>
+            <button class="boton2" onclick="cerrarSesion()">Cerrar sesión</button>
+        </div>
+    </header>
+    <main class="container">
+        <div class="login-form">
+            <h1>Bienvenido, Administrador</h1>
+            <br><br><br>
+            <h3>Elija la accion que desee realizar</h3>
+            <div class="botones">
+                <div>
+                    <form method="POST" action="asignarCertificado.php">
+                        <input type="hidden" name="id" value="<?php echo $id ?>">
+                        <input class="boton" type="submit" value="Asignar Certificado">
+                    </form>
+                    <br>
+                </div>
+                <div>
+                    
+                </div>
+                <form method="POST" action="registrarAlumno.php">
+                    <input type="hidden" name="id" value="<?php echo $id ?>">
+                    <input class="boton" type="submit" value="Registrar Alumno">
+                </form>
+                <br>
+                <form method="POST" action="verListaAlumnos.php">
+                    <input type="hidden" name="id" value="<?php echo $id ?>">
+                    <input class="boton" type="submit" value="Lista de Alumnos">
+                </form>
+            </div>
+
+
+
+
+
+            <select id="accion" class="lista-desplegable">
+
+                <option value="opcion1">Asignar certificado</option>
+                <option value="opcion2">Registrar alumno</option>
+                <option value="opcion3">Ver lista de alumnos</option>
+
+            </select>
+            <button class="boton" onclick="realizarAccion()">Realizar</button>
+
+        </div>
+
+    </main>
+    <script src="script.js"></script>
 </body>
 </html>
