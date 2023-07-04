3ad1d5128efe51ef56fde206c3e1c9777e310969
Carlos Zahid <zahidhdez@outlook.com>
2023-07-04 11:48:19 -0600

    hola4

--- Administrador/inicioAdmin.php
+++ Administrador/inicioAdmin.php
@@ -12,7 +12,7 @@ $id = $_POST['id'];
         <div class="avatar">
             <img src="https://cdn.icon-icons.com/icons2/2506/PNG/512/user_icon_150670.png" alt="Avatar">
             <span>Nombre de Usuario</span>
-            <button class="boton2" onclick="cerrarSesion()">Cerrar sesión</button>
+            <button class="boton2" onclick="redirigirALogin()">Cerrar sesión</button>
         </div>
     </header>
     <main class="container">
@@ -29,35 +29,26 @@ $id = $_POST['id'];
                     <br>
                 </div>
                 <div>
-                    
+                    <form method="POST" action="registrarAlumno.php">
+                        <input type="hidden" name="id" value="<?php echo $id ?>">
+                        <input class="boton" type="submit" value="Registrar Alumno">
+                    </form>
+                    <br>
                 </div>
-                <form method="POST" action="registrarAlumno.php">
-                    <input type="hidden" name="id" value="<?php echo $id ?>">
-                    <input class="boton" type="submit" value="Registrar Alumno">
-                </form>
-                <br>
-                <form method="POST" action="verListaAlumnos.php">
-                    <input type="hidden" name="id" value="<?php echo $id ?>">
-                    <input class="boton" type="submit" value="Lista de Alumnos">
-                </form>
+                <div>
+                    <form method="POST" action="verListaAlumnos.php">
+                        <input type="hidden" name="id" value="<?php echo $id ?>">
+                        <input class="boton" type="submit" value="Lista de Alumnos">
+                    </form>
+                </div>
             </div>
-
-
-
-
-
-            <select id="accion" class="lista-desplegable">
-
-                <option value="opcion1">Asignar certificado</option>
-                <option value="opcion2">Registrar alumno</option>
-                <option value="opcion3">Ver lista de alumnos</option>
-
-            </select>
-            <button class="boton" onclick="realizarAccion()">Realizar</button>
-
         </div>
 
     </main>
-    <script src="script.js"></script>
+    <script>
+        function redirigirALogin() {
+            window.location.href = "../login/index.php";
+        }
+    </script>
 </body>
 </html>
